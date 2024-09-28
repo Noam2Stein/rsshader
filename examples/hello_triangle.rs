931135801a689abac82f17418e171250f7aefd05
NoamStein <noamsteindev@gmail.com>
2024-09-28 08:59:21 +0300

    Update hello_triangle.rs

--- examples/hello_triangle.rs
+++ examples/hello_triangle.rs
@@ -9,23 +9,23 @@ struct Vertex {
 struct Fragment {
     #[fragment_pos]
     pos: Vec4,
-    color: Vec4,
+    color: Vec3,
 }
 
 #[gpu(vertex_fn)]
 fn vs_main(vertex: Vertex) -> Fragment {
     Fragment {
         pos: vec4((vertex.pos, 0.0, 1.0)),
-        color: vec4((vertex.color, 1.0)),
+        color: vertex.color,
     }
 }
 #[gpu(fragment_fn)]
 fn fs_main(fragment: Fragment) -> Vec4 {
-    fragment.color
+    vec4((fragment.color, 1.0))
 }
 
 const HELLO_TRIANGLE: RenderPipeline<Vertex> =
-    RenderPipeline::new::<vs_main_GPUFn, fs_main_GPUFn>().optimize::<Wgsl>();
+    RenderPipeline::new::<gpufn!(vs_main), gpufn!(fs_main)>().optimize::<Wgsl>();
 
 fn main() {
     println!("{}", HELLO_TRIANGLE.format::<Wgsl>().as_str())
